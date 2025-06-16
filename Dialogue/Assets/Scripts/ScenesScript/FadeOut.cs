e1eca44251ed7cdc73ff1f444208435c2abaf6ca
eliza <elizanabibaks@gmail.com>
2025-06-16 10:25:43 +0200

    fixed error with animation(removed unused event)

---
+++ Dialogue/Assets/Scripts/ScenesScript/FadeOut.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class FadeOut : MonoBehaviour
+{
+    public Animator animator;
+    public float duration = 1.0f;
+    public string FadeOutName = "FadeOut";
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeLoadScene()
+    {
+        animator.SetTrigger(FadeOutName);
+    }
+}*/
